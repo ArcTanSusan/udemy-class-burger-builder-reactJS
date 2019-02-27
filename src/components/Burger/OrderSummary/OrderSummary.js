57ef9ead4a24957700ca600575b73412cd7220ad
Susan Tan <onceuponatimeforever@gmail.com>
2019-02-27 09:55:51 -0800

    Switch orderSummary, Modal from class to functional components
    Why? We can add lifecycle hooks like componentWillUpdate() for performance
    improvement: prevent calling ordersummary on every button -/+ click

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../../hoc/Aux';
-import Button from '../../UI/Button/Button';
+import Button from '../../Navigation/SideDrawer/UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+  componentWillUpdate() {
+    console.log("OrderSummary will update");
+  };
+  
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients)
     .map(igKey => {
       return (
         <li key={igKey}>
-          <span style={{textTransformed:'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+          <span style={{textTransformed:'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
         </li>
       )
     })
@@ -19,12 +24,41 @@ const orderSummary = (props) => {
      <ul>
       {ingredientSummary}
      </ul>
-     <p><strong>Total Price: {props.totalPrice.toFixed(2)}</strong></p>
+     <p><strong>Total Price: {this.props.totalPrice.toFixed(2)}</strong></p>
      <p>Continue to checkout?</p>
-     <Button buttonType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
-     <Button buttonType='Danger' clicked={props.purchaseCancelled}>CANCEL</Button>
+     <Button buttonType='Success' clicked={this.props.purchaseContinued}>CONTINUE</Button>
+     <Button buttonType='Danger' clicked={this.props.purchaseCancelled}>CANCEL</Button>
     </Aux>
   );
-};
+  }
+}
 
-export default orderSummary;
+export default OrderSummary;
+
+// Below is functional component
+// const orderSummary = (props) => {
+//   const ingredientSummary = Object.keys(props.ingredients)
+//     .map(igKey => {
+//       return (
+//         <li key={igKey}>
+//           <span style={{textTransformed:'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+//         </li>
+//       )
+//     })
+//
+//   return (
+//     <Aux>
+//      <h3>Your Order </h3>
+//      <p>Delicious burger with following ingredients:</p>
+//      <ul>
+//       {ingredientSummary}
+//      </ul>
+//      <p><strong>Total Price: {props.totalPrice.toFixed(2)}</strong></p>
+//      <p>Continue to checkout?</p>
+//      <Button buttonType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
+//      <Button buttonType='Danger' clicked={props.purchaseCancelled}>CANCEL</Button>
+//     </Aux>
+//   );
+// };
+//
+// export default orderSummary;
--- src/components/Navigation/SideDrawer/UI/Modal/Modal.js
+++ src/components/Navigation/SideDrawer/UI/Modal/Modal.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Modal.module.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 
-const modal = (props) => {
+class Modal extends Component {
   return (
     <Aux>
     <Backdrop show={props.show} clicked={props.modalClosed} />
@@ -19,3 +19,20 @@ const modal = (props) => {
 };
 
 export default modal;
+
+// Below is functional component.
+// const modal = (props) => {
+//   return (
+//     <Aux>
+//     <Backdrop show={props.show} clicked={props.modalClosed} />
+//     <div className={classes.Modal}
+//     style={
+//       {transform: props.show ? 'translateY(0)': 'translateY(-100vh)',
+//       opacity: props.show ? '1': '0'}}>
+//       {props.children}
+//     </div>
+//     </Aux>
+//   )
+// };
+//
+// export default modal;
